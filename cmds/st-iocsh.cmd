b269309b3dd33b8e6f295c6589338887e1b6d78b
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-10-24 16:25:48 +0200

    Added PVs for data from LabView.

--- cmds/st-iocsh.cmd
+++ cmds/st-iocsh.cmd
@@ -3,7 +3,8 @@
 require mobilellrf
 
 epicsEnvSet ("IOCNAME", "ioc29-llrfmobile")
-epicsEnvSet("TOP", "/opt/epics/autosave")
+#epicsEnvSet("TOP", "/opt/epics/autosave")
+epicsEnvSet("TOP", "/tmp")
 epicsEnvSet("IOCDIR", "llrfmobile")
 
 iocshLoad("$(mobilellrf_DIR)mobilellrf.iocsh", "IP_ADDR1=192.168.10.109,PREFIX1=LLRF-1:,DEVICE1=FUNCGEN-1,ASYN_PORT1=PORT1,IP_ADDR2=192.168.10.40,PREFIX2=LLRF-Aux:,DEVICE2=ArduinoPMC:,ASYN_PORT2=PORT2,PREFIX3=LLRF-Ctrl:")
