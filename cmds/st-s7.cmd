2d83e6c1a956c5dfba7f9fe9d263e0127b7cf495
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-03-04 14:32:48 +0100

    The first working version using S7-1200 PLC.

--- cmds/st-s7.cmd
+++ cmds/st-s7.cmd
@@ -2,9 +2,10 @@
 
 require mobilellrf
 
-epicsEnvSet ("IOCNAME", "xxx-llrfmobile")
-#epicsEnvSet("TOP", "/opt/epics/autosave")
+epicsEnvSet ("IOCNAME", "ioc29-llrfmobile")
 epicsEnvSet("TOP", "/tmp")
 epicsEnvSet("IOCDIR", "llrfmobile")
 
-iocshLoad("$(mobilellrf_DIR)mobilellrf-s7.iocsh", "IP_ADDR1=192.168.10.109,PREFIX1=LLRF-1:,DEVICE1=FUNCGEN-1,ASYN_PORT1=PORT1,IP_ADDR2=192.168.10.47,PREFIX2=xxx-Aux:,DEVICE2=ArduinoPMC:,ASYN_PORT2=PORT2,PREFIX3=LLRF-Ctrl:")
+#iocshLoad("$(mobilellrf_DIR)mobilellrf-s7.iocsh", "IP_ADDR1=192.168.10.109,PREFIX1=LLRF-1:,DEVICE1=FUNCGEN-1,ASYN_PORT1=PORT1,IP_ADDR2=192.168.10.47,PREFIX2=xxx-Aux:,DEVICE2=ArduinoPMC:,ASYN_PORT2=PORT2,PREFIX3=LLRF-Ctrl:")
+
+iocshLoad("$(mobilellrf_DIR)mobilellrf-s7.iocsh", "IP_ADDR1=192.168.10.109,PREFIX1=LLRF-1:,DEVICE1=FUNCGEN-1,ASYN_PORT1=PORT1,IP_ADDR2=192.168.10.47,PREFIX2=LLRF-Ctrl:,ARDUINO_PREFIX=LLRF-Aux:,DEVICE2=ArduinoPMC:,ASYN_PORT2=PLC_PORT,PREFIX3=LLRF-Ctrl:,IP_ADDR3=192.168.10.31,ASYN_PORT3=PORT3,IP_ADDR4=192.168.10.35,ASYN_PORT4=PORT4")
--- plc/README.txt
+++ plc/README.txt
@@ -1,3 +1,4 @@
 Test1-2025-01-09.zip  The first buckup version with the PLC program controlling DO5
 
 MobileLLRF_20250303_1542.zap18 - the first version of S7-1200 substitution of ArduinoPMC
+MobileLLRF_20250304_1420.zap18 - Added external interlock signal (not interlog logic in place).
